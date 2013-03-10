2d8a35dde09266726366ea8ba64c85578fc30a9d
morgan@kinvey.com <morgan@kinvey.com@868178e2-d38b-4ba8-a603-825f91c87007>
2013-03-10 23:36:31 +0000

    added 3rd party auth fix
    git-svn-id: https://subversion.assembla.com/svn/kinvey/java-library/branches/2.0.6@5850 868178e2-d38b-4ba8-a603-825f91c87007

--- samples/kitchensink-android/src/com/kinvey/sample/kitchensink/KitchenSinkApplication.java
+++ samples/kitchensink-android/src/com/kinvey/sample/kitchensink/KitchenSinkApplication.java
@@ -28,8 +28,8 @@ public class KitchenSinkApplication extends Application {
     private Client myClient;
 
     // NOTE: When configuring push notifications you have to change the android package name of this app
-    private String pushAppKey = "kid_PTZrfDHZ6";
-    private String pushAppSecret = "2eecf5e8909043f28a90360b01440795";
+    private String pushAppKey = "your_push_key";
+    private String pushAppSecret = "your_push_secret";
 
     @Override
     public void onCreate() {
