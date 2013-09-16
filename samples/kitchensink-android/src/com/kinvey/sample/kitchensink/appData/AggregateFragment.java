2a3cf5a290107209bb6f9a9054f28e2569001c7a
edwardf <edwardf@868178e2-d38b-4ba8-a603-825f91c87007>
2013-09-16 15:52:21 +0000

    fixed #2069 adding aggregate samples to kitchensink
    git-svn-id: https://subversion.assembla.com/svn/kinvey/java-library/trunk@7954 868178e2-d38b-4ba8-a603-825f91c87007

--- samples/kitchensink-android/src/com/kinvey/sample/kitchensink/appData/AggregateFragment.java
+++ samples/kitchensink-android/src/com/kinvey/sample/kitchensink/appData/AggregateFragment.java
@@ -13,15 +13,30 @@
  */
 package com.kinvey.sample.kitchensink.appData;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.google.api.client.json.GenericJson;
+import com.kinvey.android.AsyncAppData;
+import com.kinvey.android.callback.KinveyListCallback;
+import com.kinvey.java.Query;
+import com.kinvey.java.core.KinveyClientCallback;
+import com.kinvey.sample.kitchensink.KitchenSink;
+import com.kinvey.sample.kitchensink.MyEntity;
 import com.kinvey.sample.kitchensink.R;
 import com.kinvey.sample.kitchensink.UseCaseFragment;
+
+import java.util.ArrayList;
 
 /**
  * @author edwardf
  * @since 2.0
  */
-public class AggregateFragment extends UseCaseFragment{
+public class AggregateFragment extends UseCaseFragment implements View.OnClickListener {
+
+    private Button tryIt;
+
     @Override
     public int getViewID() {
         return R.layout.feature_appdata_aggregate;
@@ -29,11 +44,42 @@ public class AggregateFragment extends UseCaseFragment{
 
     @Override
     public void bindViews(View v) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        tryIt = (Button) v.findViewById(R.id.appdata_agg_perform);
+        tryIt.setOnClickListener(this);
     }
 
     @Override
     public String getTitle() {
         return "Aggregates";
+    }
+
+    private void performAggregation(){
+
+        AsyncAppData<GenericJson[]> aggregate = getClient().appData(KitchenSink.collectionName, GenericJson[].class);
+
+        ArrayList <String> fields = new ArrayList<String>();
+        fields.add("_acl.creator");
+        Query q = new Query();
+        aggregate.count(fields,q, new KinveyListCallback<GenericJson>() {
+            @Override
+            public void onSuccess(GenericJson[] res) {
+                Toast.makeText(getSherlockActivity(), "got: " + res[0].get("_result"), Toast.LENGTH_SHORT ).show();
+
+
+            }
+
+            @Override
+            public void onFailure(Throwable error) {
+                Toast.makeText(getSherlockActivity(), "something went wrong -> " + error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == tryIt){
+            performAggregation();
+        }
     }
 }
