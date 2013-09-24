9a6b047982002bd196bf48dd71f8fa05f54f9e32
edwardf <edwardf@868178e2-d38b-4ba8-a603-825f91c87007>
2013-09-24 20:16:22 +0000

    started #2488 adding KinveyAggregateCallback and AggregateResponse wrapper, still need to add some utility functions
    git-svn-id: https://subversion.assembla.com/svn/kinvey/java-library/trunk@8055 868178e2-d38b-4ba8-a603-825f91c87007

---
+++ java-api-core/src/com/kinvey/java/model/AggregateResponse.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2013 Kinvey Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.kinvey.java.model;
+
+import com.google.api.client.json.GenericJson;
+import com.google.api.client.util.Key;
+
+import java.util.ArrayList;
+
+/**
+ * @author edwardf
+ */
+public class AggregateResponse {
+
+    public AggregateResult[] results;
+
+    public AggregateResponse(AggregateResult[] res){
+        this.results = res;
+    }
+
+    public ArrayList<Number> getResultsFor(String key, String value){
+        ArrayList<Number> ret = new ArrayList<Number>();
+        for (AggregateResult a : results){
+            if (a.containsKey(key)){
+                if (a.get(key).equals(value)){
+                    ret.add(a.result);
+                }
+            }
+        }
+        return ret;
+    }
+
+
+
+
+
+
+
+
+
+    public static class AggregateResult extends GenericJson{
+        @Key("_result")
+        public Number result;
+
+        public AggregateResult(){}
+    }
+
+
+}
+//[{"_acl.creator":"5230dea6c5cf276b1200000f","_result":3},{"_acl.creator":"520a7096519e1429230000f5","_result":1},{"_acl.creator":"5238c3a29bb2741a3800015d","_result":3}]
