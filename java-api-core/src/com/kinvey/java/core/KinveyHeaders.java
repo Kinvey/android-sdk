239abb6ef9b5bd2244bfff995104ccb9201e660c
edwardf <edwardf@868178e2-d38b-4ba8-a603-825f91c87007>
2013-10-25 01:47:18 +0000

    bumping version to 2.6.9
    git-svn-id: https://subversion.assembla.com/svn/kinvey/java-library/trunk@8283 868178e2-d38b-4ba8-a603-825f91c87007

--- java-api-core/src/com/kinvey/java/core/KinveyHeaders.java
+++ java-api-core/src/com/kinvey/java/core/KinveyHeaders.java
@@ -24,7 +24,7 @@ import com.google.api.client.util.Key;
  */
 public class KinveyHeaders extends HttpHeaders {
 
-    private String VERSION = "2.6.8";
+    private String VERSION = "2.6.9";
 
     @Key("X-Kinvey-API-Version")
     private String kinveyApiVersion = "3";
