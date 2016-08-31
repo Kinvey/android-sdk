175ef930a19f3e2d59a4c404fee82747a5394caa
Michael Vakulich <m.vakulich@softteco.com>
2016-08-31 15:23:13 +0300

    fixed java-api-core tests after UserStore changing

--- java-api-core/test/com/kinvey/java/UserTest.java
+++ java-api-core/test/com/kinvey/java/UserTest.java
@@ -24,6 +24,7 @@ import com.kinvey.java.auth.ThirdPartyIdentity;
 import com.kinvey.java.core.KinveyMockUnitTest;
 import com.kinvey.java.dto.User;
 import com.kinvey.java.store.UserStore;
+import com.kinvey.java.store.UserStoreRequestManager;
 import com.kinvey.java.store.requests.user.Delete;
 import com.kinvey.java.store.requests.user.EmailVerification;
 import com.kinvey.java.store.requests.user.GetMICAccessToken;
@@ -39,15 +40,15 @@ import com.kinvey.java.testing.MockKinveyAuthRequest;
  */
 public class UserTest extends KinveyMockUnitTest {
 
-    private UserStore<User> currentUser;
+    private UserStoreRequestManager<User> requestManager;
 
-    private void initializeUser() {
-        currentUser = new UserStore<User>(getClient(), User.class, new MockKinveyAuthRequest.MockBuilder(getClient().getRequestFactory().getTransport(),
+    private void initializeRequestManager() {
+        requestManager = new UserStoreRequestManager<User>(getClient(), User.class, new MockKinveyAuthRequest.MockBuilder(getClient().getRequestFactory().getTransport(),
                 getClient().getJsonFactory(), "mockAppKey","mockAppSecret",null));
     }
 
     public void testInitializeUser() {
-        UserStore<User> user = new UserStore<User>(getClient(), User.class, new MockKinveyAuthRequest.MockBuilder(getClient().getRequestFactory().getTransport(),
+        UserStoreRequestManager<User> user = new UserStoreRequestManager<User>(getClient(), User.class, new MockKinveyAuthRequest.MockBuilder(getClient().getRequestFactory().getTransport(),
                 getClient().getJsonFactory(), "mockAppKey","mockAppSecret",null));
         assertNotNull(user);
         assertEquals(getClient(),user.getClient());
@@ -56,7 +57,7 @@ public class UserTest extends KinveyMockUnitTest {
 
     public void testInitializeUserNullClient() {
         try {
-            UserStore<User> user = new UserStore<User>(null, User.class, new MockKinveyAuthRequest.MockBuilder(getClient().getRequestFactory().getTransport(),
+            UserStoreRequestManager<User> user = new UserStoreRequestManager<User>(null, User.class, new MockKinveyAuthRequest.MockBuilder(getClient().getRequestFactory().getTransport(),
                     getClient().getJsonFactory(), "mockAppKey","mockAppSecret",null));
             fail("NullPointerException should be thrown");
         } catch (NullPointerException ex) {}
@@ -64,240 +65,246 @@ public class UserTest extends KinveyMockUnitTest {
 
     public void testInitializeNoBuilder() {
         try {
-            UserStore<User> user = new UserStore<User>(getClient(), User.class, null);
+            UserStoreRequestManager<User> user = new UserStoreRequestManager<User>(getClient(), User.class, null);
             fail("NullPointerException should be thrown");
         } catch (NullPointerException ex) {}
     }
 
 
     public void testLoginKinveyUserNullUsername() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            currentUser.loginBlocking(null, "myPassword").execute();
+            requestManager.loginBlocking(null, "myPassword").execute();
             fail("NullPointerException should be thrown");
         } catch (NullPointerException ex) {}
     }
 
     public void testLoginKinveyUserNullPassword() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            currentUser.loginBlocking("myUserName", null).execute();
+            requestManager.loginBlocking("myUserName", null).execute();
             fail("NullPointerException should be thrown");
         } catch (NullPointerException ex) {}
     }
 
 
     public void testLoginFacebookUserNullArguments() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            currentUser.login(ThirdPartyIdentity.Type.FACEBOOK, null).execute();
+            requestManager.login(ThirdPartyIdentity.Type.FACEBOOK, null).execute();
             fail("NullPointerException should be thrown");
         } catch (NullPointerException ex) {}
     }
 
     public void testLoginFacebookTooFewArguments() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            currentUser.login(ThirdPartyIdentity.Type.FACEBOOK, new String[] {}).execute();
+            requestManager.login(ThirdPartyIdentity.Type.FACEBOOK, new String[] {}).execute();
             fail("IllegalArgumentException should be thrown");
         } catch (IllegalArgumentException ex) {}
     }
 
     public void testLoginFacebookTooManyArguments() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            currentUser.login(ThirdPartyIdentity.Type.FACEBOOK, new String[] {"arg1","arg2"}).execute();
+            requestManager.login(ThirdPartyIdentity.Type.FACEBOOK, new String[] {"arg1","arg2"}).execute();
             fail("IllegalArgumentException should be thrown");
         } catch (IllegalArgumentException ex) {}
     }
 
 
+
     public void testDeleteHardDeleteTrue() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-        Delete del = currentUser.deleteBlocking(true);
-        assertEquals(currentUser.getCurrentUser().getId(), del.get("userID").toString());
+        initializeRequestManager();
+        User user = requestManager.getClient().getUser();
+        user.setId("testUser");
+        Delete del = requestManager.deleteBlocking(true);
+        assertEquals(requestManager.getClient().getUser().getId(), del.get("userID").toString());
         assertEquals(true, del.get("hard"));
     }
 
     public void testDeleteHardDeleteFalse() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-        Delete del = currentUser.deleteBlocking(false);
-        assertEquals(currentUser.getCurrentUser().getId(),del.get("userID").toString());
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
+        Delete del = requestManager.deleteBlocking(false);
+        assertEquals(requestManager.getClient().getUser().getId(),del.get("userID").toString());
         assertEquals(false,del.get("hard"));
         assertEquals("DELETE",del.getRequestMethod());
     }
 
     public void testDeleteNullUser() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            Delete del = currentUser.deleteBlocking(true);
+            Delete del = requestManager.deleteBlocking(true);
             fail("NullPointerException should be thrown.");
         } catch (NullPointerException ex) {}
     }
 
     public void testRetrieve() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-        Retrieve ret = currentUser.retrieveBlocking();
-        assertEquals(currentUser.getCurrentUser().getId(),ret.get("userID").toString());
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
+        Retrieve ret = requestManager.retrieveBlocking();
+        assertEquals(requestManager.getClient().getUser().getId(),ret.get("userID").toString());
         assertEquals("GET", ret.getRequestMethod());
     }
 
     public void testRetrieveNullUser() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            Retrieve ret = currentUser.retrieveBlocking();
+            Retrieve ret = requestManager.retrieveBlocking();
             fail("NullPointerException should be thrown.");
         } catch (NullPointerException ex) {}
     }
 
     public void testUpdate() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-        Update update = currentUser.updateBlocking();
-        assertEquals(currentUser.getCurrentUser().getId(),update.get("userID").toString());
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
+        Update update = requestManager.updateBlocking();
+        assertEquals(requestManager.getClient().getUser().getId(),update.get("userID").toString());
         assertEquals("PUT", update.getRequestMethod());
     }
 
     public void testUpdateNullUser() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            Update update = currentUser.updateBlocking();
+            Update update = requestManager.updateBlocking();
             fail("NullPointerException should be thrown.");
         } catch (NullPointerException ex) {}
     }
 
     public void testResetPassword() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-        currentUser.getCurrentUser().setUsername("test");
-        ResetPassword pwd = currentUser.resetPasswordBlocking(currentUser.getCurrentUser().getUsername());
-        assertEquals(currentUser.getCurrentUser().getUsername(),pwd.get("userID").toString());
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
+        requestManager.getClient().getUser().setUsername("test");
+        ResetPassword pwd = requestManager.resetPasswordBlocking(requestManager.getClient().getUser().getUsername());
+        assertEquals(requestManager.getClient().getUser().getUsername(),pwd.get("userID").toString());
         assertEquals("POST", pwd.getRequestMethod());
     }
 
     public void testResetPasswordNullUser() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            ResetPassword pwd = currentUser.resetPasswordBlocking(null);
+            ResetPassword pwd = requestManager.resetPasswordBlocking(null);
             fail("NullPointerException should be thrown.");
         } catch (NullPointerException ex) {}
     }
 
     public void testEmailVerification() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-        EmailVerification email = currentUser.sendEmailVerificationBlocking();
-        assertEquals(currentUser.getCurrentUser().getId(),email.get("userID").toString());
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
+        EmailVerification email = requestManager.sendEmailVerificationBlocking();
+        assertEquals(requestManager.getClient().getUser().getId(),email.get("userID").toString());
         assertEquals("POST", email.getRequestMethod());
     }
 
     public void testEmailVerificationNullUser() throws IOException {
-        initializeUser();
+        initializeRequestManager();
         try {
-            EmailVerification email = currentUser.sendEmailVerificationBlocking();
+            EmailVerification email = requestManager.sendEmailVerificationBlocking();
             fail("NullPointerException should be thrown.");
         } catch (NullPointerException ex) {}
     }
-    
+
     public void testUserCustomVersion() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-    	currentUser.getClient().setClientAppVersion("1.2.3");
-    	Retrieve request = currentUser.retrieveBlocking();
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
+    	requestManager.getClient().setClientAppVersion("1.2.3");
+    	Retrieve request = requestManager.retrieveBlocking();
     	Object header = request.getRequestHeaders().get("X-Kinvey-Client-App-Version");
     	assertEquals("1.2.3", (String) header);
     }
 
     public void testUserCustomVesionAsNumber() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-        currentUser.getClient().setClientAppVersion(1, 2, 3);
-        Retrieve request = currentUser.retrieveBlocking();
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
+        requestManager.getClient().setClientAppVersion(1, 2, 3);
+        Retrieve request = requestManager.retrieveBlocking();
         Object header = request.getRequestHeaders().get("X-Kinvey-Client-App-Version");
         assertEquals("1.2.3", (String) header);
 
     }
-    
+
     public void testUserCustomHeader() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
     	GenericJson custom = new GenericJson();
     	custom.put("First", 1);
     	custom.put("Second", "two");
-    	currentUser.getClient().setCustomRequestProperties(custom);
-    	Retrieve request = currentUser.retrieveBlocking();
+    	requestManager.getClient().setCustomRequestProperties(custom);
+    	Retrieve request = requestManager.retrieveBlocking();
     	Object header = request.getRequestHeaders().get("X-Kinvey-Custom-Request-Properties");
-    	assertEquals("{\"First\":1,\"Second\":\"two\"}", (String) header);    	
-    	
+    	assertEquals("{\"First\":1,\"Second\":\"two\"}", (String) header);
+
     }
 
     public void testUserCustomHeaderOverload() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
 
-        currentUser.getClient().setCustomRequestProperty("First", 1);
-        currentUser.getClient().setCustomRequestProperty("Second", "two");
+        requestManager.getClient().setCustomRequestProperty("First", 1);
+        requestManager.getClient().setCustomRequestProperty("Second", "two");
 
-        Retrieve request = currentUser.retrieveBlocking();
+        Retrieve request = requestManager.retrieveBlocking();
         Object header = request.getRequestHeaders().get("X-Kinvey-Custom-Request-Properties");
         assertEquals("{\"First\":1,\"Second\":\"two\"}", (String) header);
 
     }
-    
+
     public void testUserCustomVersionNull() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-    	currentUser.getClient().setClientAppVersion(null);
-    	Retrieve request = currentUser.retrieveBlocking();
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
+    	requestManager.getClient().setClientAppVersion(null);
+    	Retrieve request = requestManager.retrieveBlocking();
     	Object header = request.getRequestHeaders().get("X-Kinvey-Client-App-Version");
-    	assertEquals(null, header);    	
+    	assertEquals(null, header);
     }
-    
+
     public void testUserCustomHeaderNull() throws IOException {
-        initializeUser();
-        currentUser.getCurrentUser().setId("testUser");
-        currentUser.getClient().clearCustomRequestProperties();
-    	Retrieve request = currentUser.retrieveBlocking();
+        initializeRequestManager();
+        requestManager.getClient().getUser().setId("testUser");
+        requestManager.getClient().clearCustomRequestProperties();
+    	Retrieve request = requestManager.retrieveBlocking();
     	Object header = request.getRequestHeaders().get("X-Kinvey-Custom-Request-Properties");
-    	assertEquals(null, header);      	
+    	assertEquals(null, header);
     }
-    
+
     public void testCustomMICBase() throws IOException{
-    	initializeUser();
-    	getClient().userStore().setMICHostName("https://www.google.com");
-    	
+    	initializeRequestManager();
+    	getClient().setMICHostName("https://www.google.com");
+
     	try{
-    		getClient().userStore().setMICHostName("http://www.google.com");
+    		getClient().setMICHostName("http://www.google.com");
     		fail("Library should throw an exception when setting non https base url for MIC");
     	}catch(Exception e){}
 
 
-    	
-    	GetMICAccessToken getToken = getClient().userStore().getMICToken("myCODE");
+
+    	GetMICAccessToken getToken = requestManager.getMICToken("myCODE");
     	assertEquals("https://www.google.com/oauth/token", getToken.buildHttpRequest().getUrl().toString());
     }
-    
+
     public void testMICLoginWithAccessToken() throws IOException{
-    	
-    	currentUser = new UserStore<>(getClient(new MockHttpForMIC()), User.class, new KinveyAuthRequest.Builder(new MockHttpForMIC(),
+
+    	requestManager = new UserStoreRequestManager<>(getClient(new MockHttpForMIC()), User.class, new KinveyAuthRequest.Builder(new MockHttpForMIC(),
                 new GsonFactory(),"https://baas.kinvey.com",  "mockAppKey","mockAppSecret",null));
-    	
-    	GetMICAccessToken token = currentUser.getMICToken("MyToken");
+
+    	GetMICAccessToken token = requestManager.getMICToken("MyToken");
     	GenericJson result =  (GenericJson) token.execute();
-	
-		User ret =  currentUser.loginMobileIdentityBlocking(result.get("access_token").toString()).execute();
-		  
-    	assertEquals(true, currentUser.isUserLoggedIn());
-    	
+
+        if (getClient().isUserLoggedIn()) {
+            requestManager.logout().execute();
+        }
+        User ret = requestManager.loginMobileIdentityBlocking(result.get("access_token").toString()).execute();
+
+
+    	assertEquals(true, getClient().isUserLoggedIn());
+
     }
 
     public void testMICAPIVersionAppendsV() throws IOException{
-        initializeUser();
-        currentUser.setMICApiVersion("2");
-        assertEquals(currentUser.MICApiVersion, "v2");
+        initializeRequestManager();
+        getClient().setMICApiVersion("2");
+        assertEquals(getClient().getMICApiVersion(), "v2");
     }
 
 
