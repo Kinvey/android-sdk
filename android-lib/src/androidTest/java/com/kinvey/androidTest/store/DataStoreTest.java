5344ee63534fc3957501cc9faf8566dbedd679e1
Michael Vakulich <m.vakulich@softteco.com>
2016-10-10 18:45:09 +0300

    adding testPull

--- android-lib/src/androidTest/java/com/kinvey/androidTest/store/DataStoreTest.java
+++ android-lib/src/androidTest/java/com/kinvey/androidTest/store/DataStoreTest.java
@@ -10,10 +10,12 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.RenamingDelegatingContext;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
 
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.kinvey.android.Client;
+import com.kinvey.android.callback.KinveyDeleteCallback;
 import com.kinvey.android.callback.KinveyPurgeCallback;
 import com.kinvey.android.store.AsyncDataStore;
 import com.kinvey.android.store.AsyncUserStore;
@@ -23,6 +25,7 @@ import com.kinvey.android.sync.KinveyPushCallback;
 import com.kinvey.android.sync.KinveyPushResponse;
 import com.kinvey.android.sync.KinveySyncCallback;
 import com.kinvey.androidTest.model.Person;
+import com.kinvey.java.Query;
 import com.kinvey.java.core.KinveyClientCallback;
 import com.kinvey.java.dto.User;
 import com.kinvey.java.store.StoreType;
@@ -34,6 +37,8 @@ import org.junit.runner.RunWith;
 import java.io.File;
 import java.io.IOException;
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -229,6 +234,33 @@ public class DataStoreTest {
 
         @Override
         public void onSuccess(KinveyPullResponse<Person> result) {
+            this.result = result;
+            finish();
+        }
+
+        @Override
+        public void onFailure(Throwable error) {
+            this.error = error;
+            finish();
+        }
+
+        void finish() {
+            latch.countDown();
+        }
+    }
+
+    private static class DefaultKinveyDeleteCallback implements KinveyDeleteCallback {
+
+        private CountDownLatch latch;
+        Integer result;
+        Throwable error;
+
+        DefaultKinveyDeleteCallback(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        @Override
+        public void onSuccess(Integer result) {
             this.result = result;
             finish();
         }
@@ -515,16 +547,63 @@ public class DataStoreTest {
         return callback;
     }
 
+    private DefaultKinveyDeleteCallback deleteAllCollection(final AsyncDataStore<Person> store) throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final DefaultKinveyDeleteCallback callback = new DefaultKinveyDeleteCallback(latch);
+        new Thread(new Runnable() {
+            public void run() {
+                Looper.prepare();
+                Query query = client.query();
+                query.notEqual("age", "100500");
+                store.delete(query, callback);
+                Looper.loop();
+            }
+        }).start();
+        latch.await();
+        return callback;
+    }
+
     @Test
     public void testPull() throws InterruptedException {
-/*        AsyncDataStore<Person> store = client.dataStore(Person.COLLECTION, Person.class, StoreType.SYNC);
+        AsyncDataStore<Person> store = client.dataStore(Person.COLLECTION, Person.class, StoreType.SYNC);
         client.getSycManager().clear(Person.COLLECTION);
-        pull(store);
-        assertTrue(client.getSycManager().getCount(Person.COLLECTION) == 1);
+
+        //clearing backend store
+        DefaultKinveySyncCallback syncCallback = sync(store, 120);
+        assertNull(syncCallback.error);
+        DefaultKinveyDeleteCallback deleteCallback = deleteAllCollection(store);
+        assertNull(deleteCallback.error);
+        Log.d("testPull", " : clearing backend store successful");
+        DefaultKinveyPushCallback pushCallback = push(store, 120);
+        assertNull(pushCallback.error);
+
+
+        // uploading 3 person to backend
+        ArrayList<Person> persons = new ArrayList<>();
+        Person victor = createPerson("Victor_" + UUID.randomUUID().toString());
+        Person hugo = createPerson("Hugo_" + UUID.randomUUID().toString());
+        Person barros = createPerson("Barros_" + UUID.randomUUID().toString());
+        persons.add(victor);
+        persons.add(hugo);
+        persons.add(barros);
+        for (Person person : persons) {
+            save(store, person);
+        }
+        pushCallback = push(store, 120);
+        assertNull(pushCallback.error);
+        assertNotNull(pushCallback.result);
+        Log.d("testPull", " : uploading 3 person to backend successful");
+
+        //clearing cache store
+        client.getSycManager().clear(Person.COLLECTION);
+
+        //pulling data from backend
         DefaultKinveyPullCallback pullCallback = pull(store);
         assertNull(pullCallback.error);
         assertNotNull(pullCallback.result);
-        assertTrue(client.getSycManager().getCount(Person.COLLECTION) == 0);*/
+        assertTrue(pullCallback.result.getResult().size() == 3);
+        Log.d("testPull", " : pulling data from backend successful");
+
     }
 
 
