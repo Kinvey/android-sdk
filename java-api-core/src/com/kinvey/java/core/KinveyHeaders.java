f7cc7a03d5cb9fedbca0aa0fb911b671a9203d28
Misha Vakulich <m.vakulich@softteco.com>
2017-01-27 19:07:51 +0300

    up lib version

--- java-api-core/src/com/kinvey/java/core/KinveyHeaders.java
+++ java-api-core/src/com/kinvey/java/core/KinveyHeaders.java
@@ -24,7 +24,7 @@ import com.google.api.client.util.Key;
  */
 public class KinveyHeaders extends HttpHeaders {
 
-    public static final String VERSION = "2.10.10";
+    public static final String VERSION = "2.10.11";
 
     @Key("X-Kinvey-api-Version")
     private String kinveyApiVersion = "3";
