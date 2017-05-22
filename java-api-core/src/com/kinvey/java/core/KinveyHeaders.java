33147e366713af8f88c79bd246641fb6271a8560
Misha Vakulich <m.vakulich@softteco.com>
2017-05-22 17:55:48 +0300

    bump 2.x version

--- java-api-core/src/com/kinvey/java/core/KinveyHeaders.java
+++ java-api-core/src/com/kinvey/java/core/KinveyHeaders.java
@@ -24,7 +24,7 @@ import com.google.api.client.util.Key;
  */
 public class KinveyHeaders extends HttpHeaders {
 
-    public static final String VERSION = "2.10.12";
+    public static final String VERSION = "2.10.13";
 
     @Key("X-Kinvey-api-Version")
     private String kinveyApiVersion = "3";
