224d0fa5b662066ad6d5ba895687f895f4ace2a8
Yuliya Guseva <yulia.guseva@softteco.com>
2017-08-28 16:10:51 +0300

    feat: Add FileStore tests
    MLIBZ-2006

--- android-lib/src/androidTest/java/com/kinvey/androidTest/store/EncryptionTest.java
+++ android-lib/src/androidTest/java/com/kinvey/androidTest/store/EncryptionTest.java
@@ -8,6 +8,8 @@ import android.test.RenamingDelegatingContext;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import com.kinvey.android.Client;
+import com.kinvey.android.callback.AsyncDownloaderProgressListener;
+import com.kinvey.android.callback.AsyncUploaderProgressListener;
 import com.kinvey.android.model.User;
 import com.kinvey.android.store.DataStore;
 import com.kinvey.android.store.FileStore;
@@ -16,6 +18,9 @@ import com.kinvey.androidTest.LooperThread;
 import com.kinvey.androidTest.model.Person;
 import com.kinvey.java.KinveyException;
 import com.kinvey.java.core.KinveyClientCallback;
+import com.kinvey.java.core.MediaHttpDownloader;
+import com.kinvey.java.core.MediaHttpUploader;
+import com.kinvey.java.model.FileMetaData;
 import com.kinvey.java.store.StoreType;
 
 import org.junit.After;
@@ -24,6 +29,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.SecureRandom;
 import java.util.concurrent.CountDownLatch;
@@ -43,6 +50,7 @@ public class EncryptionTest {
     private static final String USERNAME = "test";
     private static final String PASSWORD = "test";
     private static final String ACCESS_ERROR = "Access Error";
+    private static final String TEST_FILENAME = "test.xml";
 
     private Client client;
     private Context mContext;
@@ -101,6 +109,96 @@ public class EncryptionTest {
         }
     }
 
+    private static class DefaultUploadProgressListener implements AsyncUploaderProgressListener<FileMetaData> {
+        private CountDownLatch latch;
+        private FileMetaData fileMetaDataResult;
+        private Throwable error;
+        private boolean isCancelled = false;
+        private boolean onCancelled = false;
+        private int progressChangedCounter = 0;
+
+        private DefaultUploadProgressListener(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        @Override
+        public void progressChanged(MediaHttpUploader uploader) throws IOException {
+            progressChangedCounter++;
+        }
+
+        @Override
+        public void onCancelled() {
+            onCancelled = true;
+            finish();
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return isCancelled;
+        }
+
+        @Override
+        public void onSuccess(FileMetaData result) {
+            this.fileMetaDataResult = result;
+            finish();
+        }
+
+        @Override
+        public void onFailure(Throwable error) {
+            this.error = error;
+            finish();
+        }
+
+        private void finish() {
+            latch.countDown();
+        }
+    }
+
+    private static class DefaultDownloadProgressListener implements AsyncDownloaderProgressListener<FileMetaData> {
+
+        private CountDownLatch latch;
+        private FileMetaData fileMetaDataResult;
+        private Throwable error;
+        private boolean isCancelled = false;
+        private boolean onCancelled = false;
+        private int progressChangedCounter = 0;
+
+        private DefaultDownloadProgressListener(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        @Override
+        public void progressChanged(MediaHttpDownloader downloader) throws IOException {
+            progressChangedCounter++;
+        }
+
+        @Override
+        public void onCancelled() {
+            onCancelled = true;
+            finish();
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return isCancelled;
+        }
+
+        @Override
+        public void onSuccess(FileMetaData result) {
+            this.fileMetaDataResult = result;
+            finish();
+        }
+
+        @Override
+        public void onFailure(Throwable error) {
+            this.error = error;
+            finish();
+        }
+
+        private void finish() {
+            latch.countDown();
+        }
+    }
     
     @Before
     public void setUp() throws InterruptedException, IOException {
@@ -241,6 +339,98 @@ public class EncryptionTest {
         Client secondClient = new Client.Builder(mContext).setEncryptionKey(key).build();
         FileStore secondFileStore = secondClient.getFileStore(StoreType.SYNC);
         assertNotNull(secondFileStore);
+    }
+
+    @Test
+    public void testFileStoreEncryptionUploading() throws InterruptedException, IOException {
+        byte[] key = new byte[64];
+        new SecureRandom().nextBytes(key);
+        client = new Client.Builder(mContext).setEncryptionKey(key).build();
+        UserKinveyClientCallback callback = login(USERNAME, PASSWORD);
+        assertNull(callback.error);
+        assertNotNull(callback.result);
+        FileStore fileStore = client.getFileStore(StoreType.SYNC);
+        File file = createFile();
+        DefaultUploadProgressListener listener = uploadFileWithMetadata(fileStore, file, testMetadata());
+        assertNotNull(listener.fileMetaDataResult);
+        assertNull(listener.error);
+        file.delete();
+    }
+
+    @Test
+    public void testFileStoreEncryptionUploadingDownloading() throws InterruptedException, IOException {
+        byte[] key = new byte[64];
+        new SecureRandom().nextBytes(key);
+        client = new Client.Builder(mContext).setEncryptionKey(key).build();
+        UserKinveyClientCallback callback = login(USERNAME, PASSWORD);
+        assertNull(callback.error);
+        assertNotNull(callback.result);
+        FileStore fileStore = client.getFileStore(StoreType.SYNC);
+        File file = createFile();
+        DefaultUploadProgressListener listener = uploadFileWithMetadata(fileStore, file, testMetadata());
+        assertNotNull(listener.fileMetaDataResult);
+        assertNull(listener.error);
+        file.delete();
+        Client secondClient = new Client.Builder(mContext).setEncryptionKey(key).build();
+        FileStore secondFileStore = secondClient.getFileStore(StoreType.SYNC);
+        assertNotNull(secondFileStore);
+        DefaultDownloadProgressListener downloadListener = downloadFile(secondFileStore, listener.fileMetaDataResult);
+        assertNotNull(downloadListener.fileMetaDataResult);
+        assertNull(downloadListener.error);
+    }
+
+    private File createFile() throws IOException {
+        File file = new File(client.getContext().getFilesDir(), TEST_FILENAME);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        return file;
+    }
+
+    private FileMetaData testMetadata() {
+        final FileMetaData fileMetaData = new FileMetaData();
+        fileMetaData.setFileName(TEST_FILENAME);
+        return fileMetaData;
+    }
+
+    private DefaultDownloadProgressListener downloadFile(final FileStore fileStore, final FileMetaData metaFile) throws InterruptedException, IOException {
+        final CountDownLatch downloadLatch = new CountDownLatch(1);
+        final DefaultDownloadProgressListener listener = new DefaultDownloadProgressListener(downloadLatch);
+        LooperThread looperThread = new LooperThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final FileOutputStream fos = new FileOutputStream(createFile());
+                    fileStore.download(metaFile, fos, listener, null);
+                } catch (IOException e) {
+                    listener.onFailure(e);
+                }
+            }
+        });
+        looperThread.start();
+        downloadLatch.await();
+        looperThread.mHandler.sendMessage(new Message());
+        return listener;
+    }
+
+    public DefaultUploadProgressListener uploadFileWithMetadata(final  FileStore fileStore, final File f, final FileMetaData metaData) throws InterruptedException, IOException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final DefaultUploadProgressListener listener = new DefaultUploadProgressListener(latch);
+        LooperThread looperThread = new LooperThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    fileStore.upload(f, metaData, listener);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    listener.onFailure(e);
+                }
+            }
+        });
+        looperThread.start();
+        latch.await();
+        looperThread.mHandler.sendMessage(new Message());
+        return listener;
     }
 
     private UserKinveyClientCallback login(final String userName, final String password) throws InterruptedException {
