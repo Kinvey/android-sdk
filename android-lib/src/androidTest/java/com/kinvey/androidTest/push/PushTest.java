7201e28478137d942c9b406f71136100c6760694
Yuliya Guseva <yulia.guseva@softteco.com>
2018-07-18 17:21:51 +0300

    feat: Uncomment testGCMPush
    MLIBZ-2607

--- android-lib/src/androidTest/java/com/kinvey/androidTest/push/PushTest.java
+++ android-lib/src/androidTest/java/com/kinvey/androidTest/push/PushTest.java
@@ -94,7 +94,6 @@ public class PushTest {
     }
 
     @Test
-    @Ignore
     public void testGCMPush() throws InterruptedException {
         if (!client.isUserLoggedIn()) {
             testManager.login(USERNAME, PASSWORD, client);
