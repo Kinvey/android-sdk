e1f4d7cfaf0b05497449ff256a4010bbb859115a
Bogdan Stolyarov <b.stolyarov@softteco.com>
2019-07-24 22:43:37 +0300

    MLIBZ-3100, Converted AsyncBatchPushRequest

--- java-api-core/src/com/kinvey/java/model/KinveySyncSaveBatchResponse.kt
+++ java-api-core/src/com/kinvey/java/model/KinveySyncSaveBatchResponse.kt
@@ -4,5 +4,5 @@ import com.google.api.client.json.GenericJson
 import com.google.api.client.util.Key
 
 class KinveySyncSaveBatchResponse<T>(
-    var entityList: List<T>?,
+    var entityList: Collection<T?>?,
     var errors: List<KinveyBatchInsertError>?) : GenericJson()
