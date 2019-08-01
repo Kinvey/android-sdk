e7c5c3b9947e392578701d1fe4bcc5e63ada9dbe
b-stolyarov <45175448+b-stolyarov@users.noreply.github.com>
2019-08-01 17:52:17 +0300

    MLIBZ-3091: Add Android Lint to project (#246)
    * added lint task to build process
    * MLIBZ-3091, added fixes for lint

--- android-lib/src/main/java/com/kinvey/android/UuidFactory.java
+++ android-lib/src/main/java/com/kinvey/android/UuidFactory.java
@@ -24,8 +24,10 @@ import com.google.gson.Gson;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings.Secure;
+import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
 
 /**
@@ -77,9 +79,8 @@ class UuidFactory {
 
                     } else {
 
-                        final String androidId = Secure
-                                .getString(context.getContentResolver(),
-                                        Secure.ANDROID_ID);
+                        final String androidId =
+                              Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
 
                         // Use the Android ID unless it's broken, in which case
                         // fallback on deviceId, unless it's not available, then
@@ -88,28 +89,31 @@ class UuidFactory {
                             if (androidId != null && !"9774d56d682e549c".equals(androidId)) {
                                 uuid = UUID.nameUUIDFromBytes(androidId.getBytes("utf8"));
                             } else {
-                                final String deviceId = ((TelephonyManager) context
-                                        .getSystemService(Context.TELEPHONY_SERVICE))
-                                        .getDeviceId();
-                                uuid = deviceId != null ? UUID.nameUUIDFromBytes(deviceId.getBytes("utf8"))
-                                        : UUID.randomUUID();
+                                String deviceId = phoneDeviceId(context);
+                                uuid = deviceId != null ? UUID.nameUUIDFromBytes(deviceId.getBytes("utf8")) : UUID.randomUUID();
                             }
                         } catch (UnsupportedEncodingException e) {
                             throw new RuntimeException(e);
                         }
-
                         // Write the value out to the prefs file
                         prefs.edit().putString(PREFS_DEVICE_ID, uuid.toString()).commit();
-
                     }
-
                 }
             }
         }
-
     }
 
-
+    private String phoneDeviceId(Context context) {
+        String deviceId = "";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                deviceId = ((TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
+            }
+        } else {
+            deviceId = ((TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
+        }
+        return deviceId;
+    }
 
     /**
      * Returns a unique UUID for the current android device. As with all UUIDs,
