3276a14f56fbcc4696bb23bae4ed5479d31c3864
Victor Hugo Barros <hey.zooi@gmail.com>
2019-08-08 19:00:49 -0700

    upgrading build tools to 29.0.2

--- android-lib/src/main/java/com/kinvey/android/store/FileStore.kt
+++ android-lib/src/main/java/com/kinvey/android/store/FileStore.kt
@@ -49,47 +49,58 @@ class FileStore(networkFileManager: NetworkFileManager,
                 cacheManager: ICacheManager, ttl: Long?, storeType: StoreType, cacheFolder: String) : BaseFileStore(networkFileManager, cacheManager, ttl, storeType, cacheFolder) {
 
 
-    private enum class FileMethods {
-        UPLOAD_FILE,
-        UPLOAD_FILE_METADATA,
-        UPLOAD_STREAM_METADATA,
-        UPLOAD_STREAM_FILENAME,
-        REMOVE_ID,
-        DOWNLOAD_METADATA,
-        CACHED_DOWNLOAD_METADATA,
-        REFRESH_FILE,
-        FIND_QUERY
+    private enum class FileMethods(val method: Method) {
+
+        //UPLOAD METHODS
+        UPLOAD_FILE(BaseFileStore::class.java.getDeclaredMethod("upload", File::class.java, UploaderProgressListener::class.java)),
+        UPLOAD_FILE_METADATA(BaseFileStore::class.java.getDeclaredMethod("upload", File::class.java, FileMetaData::class.java, UploaderProgressListener::class.java)),
+        UPLOAD_STREAM_METADATA(BaseFileStore::class.java.getDeclaredMethod("upload", InputStream::class.java, FileMetaData::class.java, UploaderProgressListener::class.java)),
+        UPLOAD_STREAM_FILENAME(BaseFileStore::class.java.getDeclaredMethod("upload", String::class.java, InputStream::class.java, UploaderProgressListener::class.java)),
+
+        //DELETE
+        REMOVE_ID(BaseFileStore::class.java.getDeclaredMethod("remove", FileMetaData::class.java)),
+
+        //DOWNLOAD
+        DOWNLOAD_METADATA(BaseFileStore::class.java.getDeclaredMethod("download", FileMetaData::class.java, OutputStream::class.java, KinveyCachedClientCallback::class.java, DownloaderProgressListener::class.java)),
+        CACHED_DOWNLOAD_METADATA(BaseFileStore::class.java.getDeclaredMethod("download", FileMetaData::class.java, OutputStream::class.java, OutputStream::class.java, KinveyCachedClientCallback::class.java, DownloaderProgressListener::class.java)),
+
+        //REFRESH
+        REFRESH_FILE(BaseFileStore::class.java.getDeclaredMethod("refresh", FileMetaData::class.java, KinveyCachedClientCallback::class.java)),
+
+        //FIND
+        FIND_QUERY(BaseFileStore::class.java.getDeclaredMethod("find", Query::class.java, KinveyCachedClientCallback::class.java))
+
     }
 
     @Throws(IOException::class, KinveyException::class)
     fun upload(file: File, listener: AsyncUploaderProgressListener<FileMetaData>) {
-        AsyncUploadRequest(this, asyncMethods[FileMethods.UPLOAD_FILE], listener, file).execute()
+        AsyncUploadRequest(this, FileMethods.UPLOAD_FILE.method, listener, file).execute()
     }
 
     @Throws(IOException::class)
     fun upload(file: File, metadata: FileMetaData,
                listener: AsyncUploaderProgressListener<FileMetaData>) {
 
-        AsyncUploadRequest(this, asyncMethods[FileMethods.UPLOAD_FILE_METADATA], listener,
+        AsyncUploadRequest(this, FileMethods.UPLOAD_FILE_METADATA.method, listener,
                 file, metadata).execute()
     }
 
     @Throws(IOException::class)
     fun upload(`is`: InputStream, metadata: FileMetaData, listener: AsyncUploaderProgressListener<FileMetaData>) {
-        AsyncUploadRequest(this, asyncMethods[FileMethods.UPLOAD_STREAM_METADATA], listener,
+        AsyncUploadRequest(this, FileMethods.UPLOAD_STREAM_METADATA.method, listener,
                 `is`, metadata).execute()
     }
 
     @Throws(IOException::class)
     fun upload(filename: String, `is`: InputStream,
                listener: AsyncUploaderProgressListener<FileMetaData>) {
-        AsyncUploadRequest(this, asyncMethods[FileMethods.UPLOAD_STREAM_FILENAME], listener,
+        AsyncUploadRequest(this, FileMethods.UPLOAD_STREAM_FILENAME.method, listener,
                 filename, `is`).execute()
     }
 
     @Throws(IOException::class)
     fun remove(metadata: FileMetaData, callback: KinveyDeleteCallback) {
-        AsyncRequest(this, asyncMethods[FileMethods.REMOVE_ID], callback,
+        AsyncRequest(this, FileMethods.REMOVE_ID.method, callback,
                 metadata).execute()
     }
 
@@ -103,19 +114,19 @@ class FileStore(networkFileManager: NetworkFileManager,
     @JvmOverloads
     fun download(metadata: FileMetaData, os: OutputStream,
                  progressListener: AsyncDownloaderProgressListener<FileMetaData>, cachedOs: OutputStream? = null, cachedClientCallback: KinveyCachedClientCallback<FileMetaData>? = null) {
-        AsyncDownloadRequest(this, asyncMethods[FileMethods.CACHED_DOWNLOAD_METADATA], progressListener,
+        AsyncDownloadRequest(this, FileMethods.CACHED_DOWNLOAD_METADATA.method, progressListener,
                 metadata, os, cachedOs, getWrappedCacheCallback(cachedClientCallback)).execute()
     }
 
     @Throws(IOException::class)
     @JvmOverloads
     fun refresh(metadata: FileMetaData, metaCallback: KinveyClientCallback<FileMetaData>, cachedClientCallback: KinveyCachedClientCallback<FileMetaData>? = null) {
-        AsyncRequest(this, asyncMethods[FileMethods.REFRESH_FILE], metaCallback, metadata, cachedClientCallback).execute()
+        AsyncRequest(this, FileMethods.REFRESH_FILE.method, metaCallback, metadata, cachedClientCallback).execute()
     }
 
     @JvmOverloads
     fun find(q: Query, metaCallback: KinveyClientCallback<Array<FileMetaData>>, cachedClientCallback: KinveyCachedClientCallback<Array<FileMetaData>>? = null) {
-        AsyncRequest(this, asyncMethods[FileMethods.FIND_QUERY], metaCallback, q, cachedClientCallback).execute()
+        AsyncRequest(this, FileMethods.FIND_QUERY.method, metaCallback, q, cachedClientCallback).execute()
     }
 
     fun cachedFile(fileId: String): FileMetaData? {
@@ -130,6 +141,7 @@ class FileStore(networkFileManager: NetworkFileManager,
     }
 
     private class ThreadedKinveyCachedClientCallback<T> internal constructor(private val callback: KinveyCachedClientCallback<T>) : KinveyCachedClientCallback<T> {
+
         internal var handler: KinveyCallbackHandler<T>
 
 
@@ -153,44 +165,6 @@ class FileStore(networkFileManager: NetworkFileManager,
 
     companion object {
 
-        private val asyncMethods = HashMap<FileMethods, Method>()
-
-        init {
-            try {
-                //UPLOAD METHODS
-                asyncMethods[FileMethods.UPLOAD_FILE] = BaseFileStore::class.java.getDeclaredMethod("upload", File::class.java, UploaderProgressListener::class.java)
-                asyncMethods[FileMethods.UPLOAD_FILE_METADATA] = BaseFileStore::class.java.getDeclaredMethod("upload", File::class.java,
-                        FileMetaData::class.java,
-                        UploaderProgressListener::class.java)
-                asyncMethods[FileMethods.UPLOAD_STREAM_METADATA] = BaseFileStore::class.java.getDeclaredMethod("upload", InputStream::class.java,
-                        FileMetaData::class.java,
-                        UploaderProgressListener::class.java)
-                asyncMethods[FileMethods.UPLOAD_STREAM_FILENAME] = BaseFileStore::class.java.getDeclaredMethod("upload", String::class.java,
-                        InputStream::class.java,
-                        UploaderProgressListener::class.java)
-
-                //DELETE
-
-                asyncMethods[FileMethods.REMOVE_ID] = BaseFileStore::class.java.getDeclaredMethod("remove", FileMetaData::class.java)
-
-                //DOWNLOAD
-
-                asyncMethods[FileMethods.DOWNLOAD_METADATA] = BaseFileStore::class.java.getDeclaredMethod("download", FileMetaData::class.java, OutputStream::class.java, KinveyCachedClientCallback::class.java, DownloaderProgressListener::class.java)
-
-                asyncMethods[FileMethods.CACHED_DOWNLOAD_METADATA] = BaseFileStore::class.java.getDeclaredMethod("download", FileMetaData::class.java, OutputStream::class.java, OutputStream::class.java, KinveyCachedClientCallback::class.java, DownloaderProgressListener::class.java)
-
-                //REFRESH
-                asyncMethods[FileMethods.REFRESH_FILE] = BaseFileStore::class.java.getDeclaredMethod("refresh", FileMetaData::class.java, KinveyCachedClientCallback::class.java)
-
-                //FIND
-                asyncMethods[FileMethods.FIND_QUERY] = BaseFileStore::class.java.getDeclaredMethod("find", Query::class.java, KinveyCachedClientCallback::class.java)
-
-            } catch (e: NoSuchMethodException) {
-                e.printStackTrace()
-            }
-
-        }
-
         private fun <T> getWrappedCacheCallback(callback: KinveyCachedClientCallback<T>?): KinveyCachedClientCallback<T>? {
             var ret: KinveyCachedClientCallback<T>? = null
             if (callback != null) {
@@ -198,5 +172,7 @@ class FileStore(networkFileManager: NetworkFileManager,
             }
             return ret
         }
+
     }
+
 }
