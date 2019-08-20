f7d545df7c64505a25f0dd08f701d0ed45d73a35
Bogdan Stolyarov <b.stolyarov@softteco.com>
2019-08-20 20:32:05 +0300

    refactor: MLIBZ-3122, Convert all query classes, converted some of request classes in root folder and dto folder and some store classes

--- java-api-core/src/com/kinvey/java/store/WritePolicy.kt
+++ java-api-core/src/com/kinvey/java/store/WritePolicy.kt
@@ -14,12 +14,12 @@
  *
  */
 
-package com.kinvey.java.store;
+package com.kinvey.java.store
 
 /**
  * Created by Prots on 2/4/16.
  */
-public enum WritePolicy {
+enum class WritePolicy {
     LOCAL_THEN_NETWORK,
     FORCE_NETWORK,
     FORCE_LOCAL
