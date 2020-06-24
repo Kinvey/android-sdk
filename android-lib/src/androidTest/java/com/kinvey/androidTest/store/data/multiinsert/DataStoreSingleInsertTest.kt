cfa57b254a722336b099e4b76f2158a17502bb1f
Bogdan Stolyarov <bogdan.stolyarov@softteco.com>
2020-06-24 17:12:04 +0300

    feat: KDEV-784: Add tests

--- android-lib/src/androidTest/java/com/kinvey/androidTest/store/data/multiinsert/DataStoreSingleInsertTest.kt
+++ android-lib/src/androidTest/java/com/kinvey/androidTest/store/data/multiinsert/DataStoreSingleInsertTest.kt
@@ -3,20 +3,16 @@ package com.kinvey.androidTest.store.data.multiinsert
 import androidx.test.filters.LargeTest
 import androidx.test.runner.AndroidJUnit4
 import com.google.api.client.json.GenericJson
-import com.kinvey.android.Client
 import com.kinvey.android.store.DataStore
 import com.kinvey.androidTest.model.EntitySet
 import com.kinvey.androidTest.model.Person
-import com.kinvey.androidTest.network.MockMultiInsertNetworkManager
-import com.kinvey.java.KinveySaveBatchException
 import com.kinvey.java.core.KinveyJsonResponseException
 import com.kinvey.java.store.StoreType
-import com.kinvey.java.sync.dto.SyncRequest
 import com.kinvey.java.AbstractClient.Companion.kinveyApiVersion
 import com.kinvey.java.Constants._ID
+import org.junit.Assert
 
 
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -25,7 +21,7 @@ import java.util.ArrayList
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
-import org.junit.Assert.assertTrue
+import org.junit.Ignore
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -62,6 +58,9 @@ class DataStoreSingleInsertTest : BaseDataStoreMultiInsertTest() {
         val saveCallback = create(personStore, person)
         assertNull(saveCallback.error)
         assertNotNull(saveCallback.result)
+        assertNotNull(saveCallback.result!!.entities)
+        assertEquals(1, saveCallback.result!!.entities!!.size)
+        assertEquals(person.username, saveCallback.result!!.entities!![0].username)
 
         val findCallback = find(personStore, LONG_TIMEOUT)
         assertNotNull(findCallback.result)
@@ -84,7 +83,6 @@ class DataStoreSingleInsertTest : BaseDataStoreMultiInsertTest() {
         val person = createPerson(TEST_USERNAME)
         val personStore = DataStore.collection(MULTI_INSERT_COLLECTION, Person::class.java, StoreType.NETWORK, client)
         clearBackend(personStore)
-        client.syncManager.clear(MULTI_INSERT_COLLECTION)
         person[_ID] = "123"
         val saveCallback =  create(personStore, person)
         assertNull(saveCallback.error)
@@ -110,5 +108,114 @@ class DataStoreSingleInsertTest : BaseDataStoreMultiInsertTest() {
         clearBackend(personStore)
         kinveyApiVersion = "5"
     }
-   
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testCreateWithoutId() {
+        val netStore = DataStore.collection(MULTI_INSERT_COLLECTION, Person::class.java, StoreType.NETWORK, client)
+        clearBackend(netStore)
+        val person = createPerson(TEST_USERNAME)
+        val saveCallback = create(netStore, person)
+        assertNotNull(saveCallback.result)
+        assertNull(saveCallback.error)
+        val resultPerson = saveCallback.result?.entities?.get(0) as Person
+        assertNotNull(resultPerson.id)
+        assertEquals(person.username, resultPerson.username)
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testCreateListReturnErrorForInvalidCredentialsNetwork() {
+        val store = DataStore.collection(EntitySet.COLLECTION, EntitySet::class.java, StoreType.NETWORK, client)
+        clearBackend(store)
+        val entity = EntitySet()
+        entity.description = "entity 1"
+        val defaultKinveyListCallback = create(store, entity)
+        assertNotNull(defaultKinveyListCallback.error)
+        assertEquals(defaultKinveyListCallback.error?.javaClass, KinveyJsonResponseException::class.java)
+    }
+
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testTempIdSync() {
+        testTempId(StoreType.SYNC)
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testTempIdCache() {
+        testTempId(StoreType.CACHE)
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testTempIdAuto() {
+        testTempId(StoreType.AUTO)
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testTempIdNetwork() {
+        testTempId(StoreType.NETWORK)
+    }
+
+    @Throws(InterruptedException::class)
+    private fun testTempId(storeType: StoreType) {
+        val person = createPerson(TEST_USERNAME)
+        val personStore = DataStore.collection(MULTI_INSERT_COLLECTION, Person::class.java, storeType, client)
+        clearBackend(personStore)
+        client.syncManager.clear(MULTI_INSERT_COLLECTION)
+        val saveCallback = create(personStore, person)
+        assertNull(saveCallback.error)
+        assertNotNull(saveCallback.result)
+        assertNotNull(saveCallback.result!!.entities)
+        if (storeType == StoreType.SYNC) {
+            Assert.assertTrue(saveCallback.result!!.entities?.get(0)?.id?.startsWith("temp")!!)
+            val pushCallback = push(personStore, DEFAULT_TIMEOUT)
+            assertNull(pushCallback.error)
+            assertNotNull(pushCallback.result)
+        } else {
+            Assert.assertTrue(saveCallback.result!!.entities?.get(0)?.id?.startsWith("temp")!!)
+        }
+        val findCallback = find(personStore, LONG_TIMEOUT)
+        assertNotNull(findCallback.result)
+        assertNotNull(findCallback.result!!.result)
+        Assert.assertTrue(!saveCallback.result!!.entities?.get(0)?.id?.startsWith("temp")!!)
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testErrorMessageIfSameIdExistsNetwork() {
+        testErrorMessageIfSameIdExists(StoreType.NETWORK)
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testErrorMessageIfSameIdExistsAuto() {
+        testErrorMessageIfSameIdExists(StoreType.AUTO)
+    }
+
+    @Test
+    @Ignore("Should work after fixing: https://kinvey.atlassian.net/browse/KDEV-781")
+    @Throws(InterruptedException::class)
+    fun testErrorMessageIfSameIdExistsSync() {
+        testErrorMessageIfSameIdExists(StoreType.SYNC)
+    }
+
+    @Throws(InterruptedException::class)
+    fun testErrorMessageIfSameIdExists(storeType: StoreType) {
+        val personList = createPersonsList(true)
+        val netStore = DataStore.collection(MULTI_INSERT_COLLECTION, Person::class.java, storeType, client)
+        clearBackend(netStore)
+        var saveCallback = createList(netStore, personList)
+        assertNull(saveCallback.error)
+        assertNotNull(saveCallback.result)
+        Assert.assertTrue(checkPersonIfSameObjects(personList, saveCallback.result?.entities, false))
+        saveCallback = createList(netStore, personList)
+        assertNull(saveCallback.error)
+        assertNotNull(saveCallback.result?.errors)
+        assertNotNull(saveCallback.result?.errors?.get(0)?.description)
+        assertNotNull(saveCallback.result?.errors?.get(0)?.debug)
+    }
 }
